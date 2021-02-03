99f82d390d31671423d8f0b40ac1f81beb5b51ac
tarek khalaily <khalailyt@gmail.com>
2021-02-03 19:48:26 +0200

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/Swiper-Timeline'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/index.html'));});
+'/dist/Swiper-Timeline/index.html'));});
 app.listen(process.env.PORT || 8080);
